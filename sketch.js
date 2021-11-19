7d45cb76430e2bd41cc85f407b126b2b65b280cc
kanishkyoutuber <sunilkr117@gmail.com>
2021-11-19 21:20:44 +0530

    mcserver

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
-
+var gameState='onSling'
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -69,12 +69,16 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState!=='launched'){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+   
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState='launched';
 }
 
 function keyPressed(){
